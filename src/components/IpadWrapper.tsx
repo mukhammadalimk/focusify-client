9e1e312a4948ebb5e81f0d5842ca003257c7cd91
Muhammadali Sulton <mukhammadalimk@gmail.com>
2025-01-09 20:49:23 +0900

    fixed the issues in Portal for Modal to show up in the middle of the body and added background to IpadWrapper

--- src/components/IpadWrapper.tsx
+++ src/components/IpadWrapper.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const IpadWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="ipad-pro-container">
-      <div className="ipad-pro relative" id="ipad-pro-root">
+      <div
+        className="ipad-pro relative bg-white dark:bg-[#181A20]"
+        id="ipad-pro-root"
+      >
         {children}
       </div>
     </div>
--- src/components/modals/Portal.tsx
+++ src/components/modals/Portal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 
 interface PortalProps {
@@ -9,12 +9,24 @@ const Portal: React.FC<PortalProps> = ({ children }) => {
   const [portalRoot, setPortalRoot] = useState<HTMLElement | null>(null);
 
   useEffect(() => {
-    // Find the target div by its ID
-    const target = document.getElementById("ipad-pro-root");
-    setPortalRoot(target);
+    // Create modal root dynamically
+    let modalRoot = document.getElementById("modal-root");
+    if (!modalRoot) {
+      modalRoot = document.createElement("div");
+      modalRoot.id = "modal-root";
+      document.body.appendChild(modalRoot);
+    }
+    setPortalRoot(modalRoot);
+
+    // Clean up when unmounting
+    return () => {
+      if (modalRoot) {
+        document.body.removeChild(modalRoot);
+      }
+    };
   }, []);
 
-  if (!portalRoot) return null;
+  if (!portalRoot) return null; // Avoid calling createPortal until portalRoot is set
 
   return createPortal(children, portalRoot);
 };
